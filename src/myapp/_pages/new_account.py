eb5628546dff8c6441cbe3e2c9b6186301af691b
hari2357s <harikarthick289@gmail.com>
2026-01-16 11:15:07 +0530

    testing

--- src/myapp/_pages/new_account.py
+++ src/myapp/_pages/new_account.py
@@ -25,7 +25,7 @@ class NewAccountPage:
         :param self: Description
         """
         if st.button("Okayy"):
-            st.switch_page("myapp/pages/login.py")
+            st.switch_page("_pages/login.py")
 
     def __init__(self):
         """
@@ -62,7 +62,7 @@ class NewAccountPage:
                     else:
                         toast_warning("Something went wrong!")
 
-            st.page_link("myapp/pages/login.py", label="Already have an Account")
+            st.page_link("_pages/login.py", label="Already have an Account")
 
 
 def main():
