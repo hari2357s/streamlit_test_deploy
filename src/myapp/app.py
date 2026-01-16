c4c0819b5f1e42cf19b832dda6f72dc8c720c0c9
hari2357s <harikarthick289@gmail.com>
2026-01-16 11:32:06 +0530

    testing

--- src/myapp/app.py
+++ src/myapp/app.py
@@ -1,40 +1,43 @@
-"""
-Docstring for myapp.App
-"""
-
-import streamlit as st
-from myapp.common.container import Container
-from myapp.common.database.test_db import Test_Database
-from myapp.common.state_manager import StateManager
 import sys
+import streamlit as st
 
 st.write("sys.path[0] =", sys.path[0])
 
 
-def main():
-    """
-    Docstring for main
-    """
+# """
+# Docstring for myapp.App
+# """
 
-    if "Container" not in st.session_state:
-        db: Test_Database = Test_Database()
-        container: Container = Container(db)
-        st.session_state.Container = container
-
-    auth_pages = [st.Page("_pages/home.py", title="Talksy")]
-
-    pages = [
-        st.Page("_pages/login.py", title="Login"),
-        st.Page("_pages/new_account.py", title="New Account"),
-    ]
-
-    if StateManager.get_user() is not None and StateManager.get_authenticated():
-        pg = st.navigation(auth_pages, position="top")
-    else:
-        pg = st.navigation(pages, position="top")
-
-    pg.run()
+# import streamlit as st
+# from myapp.common.container import Container
+# from myapp.common.database.test_db import Test_Database
+# from myapp.common.state_manager import StateManager
 
 
-if __name__ == "__main__":
-    main()
+# def main():
+#     """
+#     Docstring for main
+#     """
+
+#     if "Container" not in st.session_state:
+#         db: Test_Database = Test_Database()
+#         container: Container = Container(db)
+#         st.session_state.Container = container
+
+#     auth_pages = [st.Page("_pages/home.py", title="Talksy")]
+
+#     pages = [
+#         st.Page("_pages/login.py", title="Login"),
+#         st.Page("_pages/new_account.py", title="New Account"),
+#     ]
+
+#     if StateManager.get_user() is not None and StateManager.get_authenticated():
+#         pg = st.navigation(auth_pages, position="top")
+#     else:
+#         pg = st.navigation(pages, position="top")
+
+#     pg.run()
+
+
+# if __name__ == "__main__":
+#     main()
