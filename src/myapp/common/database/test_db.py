9fe3ea1030fa0f92360c0cbcb970185919aaa858
hari2357s <harikarthick289@gmail.com>
2026-01-16 12:23:36 +0530

    testing

--- src/myapp/common/database/test_db.py
+++ src/myapp/common/database/test_db.py
@@ -6,7 +6,7 @@ from .database import IDatabase
 
 
 class Test_Database(IDatabase):
-    def __init__(self, db_path="data/test.db"):
+    def __init__(self, db_path="test.db"):
         self._db_path = db_path
         self._conn = sqlite3.connect(db_path, check_same_thread=False)
 
